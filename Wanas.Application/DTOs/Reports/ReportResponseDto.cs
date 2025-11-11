0341679122e3e452d1d201894e18444612ff9805
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-11 23:30:28 +0200

    Add ReportResponseDto to Domain Layer , without validations yet

---
+++ Wanas.Application/DTOs/Reports/ReportResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wanas.Domain.Enums;
+
+namespace Wanas.Application.DTOs.Reports
+{
+    public class ReportResponseDto
+    {
+        public int ReportId { get; set; }
+        public string ReorterId { get; set; } 
+
+        public ReportTarget TargetType { get; set; }
+        public string TargetId { get; set; }    
+
+        public string Reason { get; set; }
+        public DateTime CreatedAt { get; set; } 
+
+
+        public ReportStatus Status { get; set; }
+        public List<string> PhotoUrls { get; set; } = new List<string>();
+
+
+
+    }
+}
