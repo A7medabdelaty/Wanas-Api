cbc12213f08ac29fa459895e260550534512d9d6
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-15 00:42:55 +0200

    Commenting old mapping files

--- Wanas.Application/Mappings/ReportProfile.cs
+++ Wanas.Application/Mappings/ReportProfile.cs
@@ -10,24 +10,24 @@ namespace Wanas.Application.Mappings
     {
         public ReportProfile()
         {
-            // Map DTO to Entity for submission
-            CreateMap<CreateReportDto, Report>()
-                .ForMember(dest => dest.Status, opt => opt.Ignore()) // Status is set in the service or by default value (Pending)
-                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // CreatedAt is set in the service or by the database
-                .ForMember(dest => dest.ReporterId, opt => opt.Ignore()) // ReporterId comes from the service method parameter
-                .ForMember(dest => dest.ReportPhotos, opt => opt.Ignore()); // Photos are mapped in the service logic
+            //// Map DTO to Entity for submission
+            //CreateMap<CreateReportDto, Report>()
+            //    .ForMember(dest => dest.Status, opt => opt.Ignore()) // Status is set in the service or by default value (Pending)
+            //    .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // CreatedAt is set in the service or by the database
+            //    .ForMember(dest => dest.ReporterId, opt => opt.Ignore()) // ReporterId comes from the service method parameter
+            //    .ForMember(dest => dest.ReportPhotos, opt => opt.Ignore()); // Photos are mapped in the service logic
 
-            // Map Entity back to Response DTO
-            CreateMap<Report, ReportResponseDto>()
-                // Manually map the collection of ReportPhotos to a list of URLs
-                .ForMember(dest => dest.PhotoUrls, opt => opt.MapFrom(src => src.ReportPhotos.Select(p => p.URL)));
+            //// Map Entity back to Response DTO
+            //CreateMap<Report, ReportResponseDto>()
+            //    // Manually map the collection of ReportPhotos to a list of URLs
+            //    .ForMember(dest => dest.PhotoUrls, opt => opt.MapFrom(src => src.ReportPhotos.Select(p => p.URL)));
 
-            //  Photo URL from the DTO to Entity
-            CreateMap<string, ReportPhoto>()
-                .ForMember(dest => dest.URL, opt => opt.MapFrom(src => src))
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.ReportId, opt => opt.Ignore())
-                .ForMember(dest => dest.Report, opt => opt.Ignore());
+            ////  Photo URL from the DTO to Entity
+            //CreateMap<string, ReportPhoto>()
+            //    .ForMember(dest => dest.URL, opt => opt.MapFrom(src => src))
+            //    .ForMember(dest => dest.Id, opt => opt.Ignore())
+            //    .ForMember(dest => dest.ReportId, opt => opt.Ignore())
+            //    .ForMember(dest => dest.Report, opt => opt.Ignore());
         }
     }
 }
