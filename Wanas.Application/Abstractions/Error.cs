070c23e5da128f43b729ec31b5618006cc4cb88d
abdoelfeky1 <abdoelfeky9@gmail.com>
2025-11-15 16:30:38 +0200

    hi

---
+++ Wanas.Application/Abstractions/Error.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Wanas.Application.Abstractions;
+public record Error(string Code, string Description, int? StatusCode)
+{
+    public static readonly Error None = new(string.Empty, string.Empty, null);
+}
