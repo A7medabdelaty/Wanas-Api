8a226a730f7830732107644e0e6bcc0ade5d1001
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-18 13:23:41 +0200

    Add IGenerateListingService interface

---
+++ Wanas.Application/Interfaces/AI/IGenerateListingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wanas.Application.DTOs.AI;
+
+namespace Wanas.Application.Interfaces.AI
+{
+    public interface IGenerateListingService
+    {
+        Task<GeneratedListingResponseDto> GenerateListingAsync(GenerateListingRequestDto request , string ownerId);
+
+    }
+}
