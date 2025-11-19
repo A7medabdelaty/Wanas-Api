4cfd80adecdfa23d3bdaa41f6e18f63bf79cd225
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-19 19:35:27 +0200

    Add chatbot service interface and implementation
    Introduces the `IChatbotService` interface for asynchronous message handling. Implements `ChatbotService` class that utilizes `IAIProvider` to generate text responses.

---
+++ Wanas.Application/Interfaces/AI/IChatService.cs
@@ -0,0 +1,7 @@
+﻿namespace Wanas.Application.Interfaces.AI
+{
+    public interface IChatbotService
+    {
+        Task<string> SendMessageAsync(string message);
+    }
+}
---
+++ Wanas.Application/Services/ChatService .cs
@@ -0,0 +1,21 @@
+﻿
+using Wanas.Application.Interfaces.AI;
+
+namespace Wanas.Application.Services
+{
+    public class ChatbotService :IChatbotService
+    {
+        private readonly IAIProvider _openAIProvider;
+
+        public ChatbotService(IAIProvider openAIProvider)
+        {
+            _openAIProvider = openAIProvider;
+        }
+         public async Task<string> SendMessageAsync(string message)
+        {
+            return await _openAIProvider.GenerateTextAsync(message);
+        }
+
+    }
+    
+}
