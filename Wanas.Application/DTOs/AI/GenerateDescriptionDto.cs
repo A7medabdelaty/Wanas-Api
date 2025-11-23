16089ab279620f34bf16817fd3e4539a3e5068bd
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-23 17:54:42 +0200

    add dto for listing description using ai

--- Wanas.Application/DTOs/AI/GenerateDescriptionDto.cs
+++ Wanas.Application/DTOs/AI/GenerateDescriptionDto.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wanas.Application.DTOs.AI
+﻿namespace Wanas.Application.DTOs.AI
 {
-    internal class GenerateDescriptionDto
+    public class GenerateDescriptionDto
     {
+        public string Title { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int MonthlyPrice { get; set; }
+
+        public bool HasElevator { get; set; }
+        public string Floor { get; set; }
+        public int AreaInSqMeters { get; set; }
+
+        public int TotalRooms { get; set; }
+        public int AvailableRooms { get; set; }
+        public int TotalBeds { get; set; }
+        public int AvailableBeds { get; set; }
+        public int TotalBathrooms { get; set; }
+
+        public bool HasKitchen { get; set; }
+        public bool HasInternet { get; set; }
     }
 }
