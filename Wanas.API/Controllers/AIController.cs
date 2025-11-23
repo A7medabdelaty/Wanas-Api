f793630d4f92a156eaa0c09fae7578e7b16c4391
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-23 18:20:32 +0200

    update AICOntroller

--- Wanas.API/Controllers/AIController.cs
+++ Wanas.API/Controllers/AIController.cs
@@ -1,38 +1,46 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Wanas.Application.DTOs.AI;
-//using Wanas.Application.Interfaces.AI;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wanas.Application.DTOs.AI;
+using Wanas.Application.Interfaces.AI;
 
-//namespace Wanas.API.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class AIController : ControllerBase
-//    {
-//        private readonly IGenerateListingService _generateListingService;
+namespace Wanas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AIController : ControllerBase
+    {
+        //private readonly IGenerateListingService _generateListingService;
+        private readonly IListingDescriptionService _listingDescriptionService;
+        public AIController(IListingDescriptionService listingDescriptionService )
+        {
+            _listingDescriptionService = listingDescriptionService;
+        }
 
-//        public AIController(IGenerateListingService generateListingService)
-//        {
-//            _generateListingService = generateListingService;
+        ///// <summary>
+        ///// Generate listing suggestions using AI
+        ///// </summary>
+        ///// 
+        //[HttpPost("generate-listing")]
+        //[Authorize]
+        //public async Task<IActionResult> GenerateListing([FromBody] GenerateListingRequestDto request)
+        //{
+        //    var userId = User.Claims.("id")?.Value;
 
-//        }
+        //    if (userId == null) return Unauthorized();
 
-//        /// <summary>
-//        /// Generate listing suggestions using AI
-//        /// </summary>
-//        /// 
-//        [HttpPost("generate-listing")]
-//        [Authorize]
-//        public async Task<IActionResult> GenerateListing([FromBody] GenerateListingRequestDto request)
-//        {
-//            var userId = User.Claims.("id")?.Value;
+        //    var response = await _generateListingService.GenerateListingAsync(request, userId);
 
-//            if (userId == null) return Unauthorized();  
+        //    return Ok(response);
+        //}
 
-//            var response = await _generateListingService.GenerateListingAsync(request,userId);
+        [HttpPost("generate-description")]
+        [Authorize]
+        public async Task<IActionResult> GenerateDescription([FromBody] GenerateDescriptionDto dto)
+        {
+            var text = await _listingDescriptionService.GenerateDescriptionAsync(dto);
+            return Ok(text);
+        }
 
-//            return Ok(response);
-//        }
-//    }
-//}
+    }
+}
