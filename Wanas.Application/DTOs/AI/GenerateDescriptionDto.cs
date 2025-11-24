2c398c916a316d6a16390bd7618494ee0cffa57c
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2025-11-24 16:38:34 +0200

    update my generale listing description dto pased on create listing dto

--- Wanas.Application/DTOs/AI/GenerateDescriptionDto.cs
+++ Wanas.Application/DTOs/AI/GenerateDescriptionDto.cs
@@ -1,23 +1,27 @@
-﻿namespace Wanas.Application.DTOs.AI
+﻿using Wanas.Application.DTOs.Listing;
+
+namespace Wanas.Application.DTOs.AI
 {
     public class GenerateDescriptionDto
     {
         public string Title { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
+
         public int MonthlyPrice { get; set; }
 
         public bool HasElevator { get; set; }
         public string Floor { get; set; }
         public int AreaInSqMeters { get; set; }
 
-        public int TotalRooms { get; set; }
-        public int AvailableRooms { get; set; }
-        public int TotalBeds { get; set; }
-        public int AvailableBeds { get; set; }
         public int TotalBathrooms { get; set; }
 
         public bool HasKitchen { get; set; }
         public bool HasInternet { get; set; }
+        public bool HasAirConditioning { get; set; }
+        public bool IsPetFriendly { get; set; }
+        public bool IsSmokingAllowed { get; set; }
+
+        public List<CreateRoomDto> Rooms { get; set; }
     }
 }
